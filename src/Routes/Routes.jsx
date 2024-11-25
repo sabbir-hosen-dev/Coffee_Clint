2b01caf1959b7395a83e7ea305f50c8872f2b4b4
sabbir-hosen-dev <tssabbirhosen@gmail.com>
2024-11-26 02:29:11 +0600

    project setup

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,23 @@
+const { createBrowserRouter } = require("react-router-dom");
+
+
+
+const Routes = createBrowserRouter([
+  {
+    path: "/"
+  },
+  {
+    path : "/coffee"
+  },
+  {
+    path : "/add-coffee"
+  },
+  {
+    path : "/update-coffee"
+  },
+  {
+    path : "/add-coffee"
+  }
+])
+
+// export default 
